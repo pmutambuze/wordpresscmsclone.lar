c3799e80d2867fdf7f3f3dc72191204362b216af
Edson Shideki Kokado <eskokado@gmail.com>
2018-02-10 11:39:31 +0000

    Form echancement

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -24,14 +24,15 @@
   <!-- Main content -->
   <section class="content">
       <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-body ">
-              {!! Form::model($post, [
-                'method' => 'POST',
-                'route' => 'backend.blog.store',
-                'files' => TRUE,
-              ])!!}
+        {!! Form::model($post, [
+          'method' => 'POST',
+          'route' => 'backend.blog.store',
+          'files' => TRUE,
+          'id' => 'post-form',
+        ])!!}
+          <div class="col-xs-9">
+            <div class="box">
+              <div class="box-body ">
                 <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}" >
                   {!! Form::label('title') !!}
                   {!! Form::text('title', null, ['class' => 'form-control']) !!}
@@ -46,7 +47,7 @@
                     <span class="help-block">{{ $errors->first('slug') }}</span>
                   @endif
                 </div>
-                <div class="form-group">
+                <div class="form-group excerpt">
                   {!! Form::label('excerpt') !!}
                   {!! Form::textarea('excerpt', null, ['class' => 'form-control']) !!}
                 </div>
@@ -57,42 +58,115 @@
                     <span class="help-block">{{ $errors->first('body') }}</span>
                   @endif
                 </div>
+                <hr>
+              </div>
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+          <div class="col-xs-3">
+            <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Publish</h3>
+              </div>
+              <div class="box-body">
                 <div class="form-group {{ $errors->has('published_at') ? 'has-error' : '' }}">
                   {!! Form::label('published_at', 'Publish Date') !!}
-                  {!! Form::text('published_at', null, ['class' => 'form-control', 'placeholder' => 'Y-m-d H:i:s']) !!}
+                  <div class='input-group date' id='datetimepicker1'>
+                    {!! Form::text('published_at', null, ['class' => 'form-control', 'placeholder' => 'Y-m-d H:i:s']) !!}
+                    <span class="input-group-addon">
+                      <span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                  </div>
+
                   @if ($errors->has('published_at'))
                     <span class="help-block">{{ $errors->first('published_at') }}</span>
                   @endif
                 </div>
+              </div>
+              <div class="box-footer clearfix">
+                <div class="pull-left">
+                  <a id="draft-btn" class="btn btn-default">Save Draft</a>
+                </div>
+                <div class="pull-right">
+                  {!! Form::submit('Publish', ['class' => 'btn btn-primary']) !!}
+                </div>
+              </div>
+            </div>
+            <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Category</h3>
+              </div>
+              <div class="box-body">
                 <div class="form-group {{ $errors->has('category_id') ? 'has-error' : '' }}">
-                  {!! Form::label('category_id', 'Category') !!}
                   {!! Form::select('category_id', App\Category::pluck('title', 'id'), null, ['class' => 'form-control', 'placeholder' => 'Choose category']) !!}
                   @if ($errors->has('category_id'))
                     <span class="help-block">{{ $errors->first('category_id') }}</span>
                   @endif
                 </div>
+
+              </div>
+            </div>
+            <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Feature Image</h3>
+              </div>
+              <div class="box-body text-center">
                 <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
-                  {!! Form::label('image', 'Feature Image') !!}
-                  {!! Form::file('image') !!}
+                  <div class="fileinput fileinput-new" data-provides="fileinput">
+                    <div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
+                      <img src="http://placehold.it/200x150&text=No+Image" alt="...">
+                    </div>
+                    <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px;"></div>
+                    <div>
+                      <span class="btn btn-default btn-file"><span class="fileinput-new">Select image</span><span class="fileinput-exists">Change</span>{!! Form::file('image') !!}</span>
+                      <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                    </div>
+                  </div>
+
                   @if ($errors->has('image'))
                     <span class="help-block">{{ $errors->first('image') }}</span>
                   @endif
                 </div>
 
-                <hr>
-
-                {!! Form::submit('Create new post', ['class' => 'btn btn-primary']) !!}
-
-              {!! Form::close() !!}
+              </div>
             </div>
-            <!-- /.box-body -->
           </div>
-          <!-- /.box -->
-        </div>
+        {!! Form::close() !!}
       </div>
     <!-- ./row -->
   </section>
   <!-- /.content -->
 </div>
 
+@endsection
+
+@section('script')
+  <script type="text/javascript">
+    $('ul.pagination').addClass('no-margin pagination-sm');
+
+    $('#title').on('blur', function() {
+      var theTitle = this.value.toLowerCase().trim(),
+      slugInput = $('#slug'),
+      theSlug = theTitle.replace(/&/g, '-and-')
+                        .replace(/[^a-z0-9-]+/g, '-')
+                        .replace(/\-\-+/g,'-')
+                        .replace(/^-+|-+$/g, '-');
+      slugInput.val(theSlug);
+    });
+
+    var simplemde1 = new SimpleMDE({ element: $("#excerpt")[0] });
+    var simplemde2 = new SimpleMDE({ element: $("#body")[0] });
+
+    $('#datetimepicker1').datetimepicker({
+      format: 'YYYY-MM-DD HH:mm:ss',
+      showClear: true
+    });
+
+    $('#draft-btn').click(function(e) {
+      e.preventDefault();
+      $('#published_at').val("");
+      $('#post-form').submit();
+    })
+  </script>
 @endsection
